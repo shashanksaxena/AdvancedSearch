362e169b2b164140a5a5bac179013af3412db1f6
shashank saxena <shashanksaxena18@gmail.com>
2014-06-06 15:28:23 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<form method="post" action="search-result.php">
+<input type="text" placeholder="search this site" value="" name="query" />
+<input type="submit" value="" class="go" name="completedsearch" />
+</form>
+
+</body>
+</html>
